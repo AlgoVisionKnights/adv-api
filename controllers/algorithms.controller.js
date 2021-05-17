b278b7cdab479c9a344711626ac5e43ae207de67
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-05-17 19:10:49 -0400

    a log

--- controllers/algorithms.controller.js
+++ controllers/algorithms.controller.js
@@ -1,5 +1,7 @@
-// Retreives an algoirthm from the database
+// Retreives a specific algorithm from the database
 exports.getAlgorithm = (req, res) => {
+	// Log is for debugging purposes. Uncomment if you do not want anything on the console
+	console.log(`Request Body: ${req.body}`);
 	res.status(200);
 	res.json({
 		message: 'Hello there -Obi Wan',
